6d5759ed3c5bd437b8419c217ed560d944ad59e7
SABAVATH-GANESH <157608168+SABAVATH-GANESH@users.noreply.github.com>
2026-09-18 23:54:45 +0530

    Time: 19 ms (81.09%) | Memory: 17.5 MB (26.60%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -26,15 +26,15 @@ public:
                         ans.push_back({nums[i], nums[j], nums[left], nums[right]});
 
                         left++;
-                        // right--;
+                        right--;
 
                         while(left < right && nums[left] == nums[left-1]) {
                             left++;
                         }
 
-                        // while(left < right && nums[right] == nums[right+1]) {
-                        //     right--;
-                        // }
+                        while(left < right && nums[right] == nums[right+1]) {
+                            right--;
+                        }
                     }
                     else if(total < target) {
                         left++;
