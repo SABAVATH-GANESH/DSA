a3d9b1668df2ade2d139257bff8f0e87cf1b47dd
SABAVATH-GANESH <157608168+SABAVATH-GANESH@users.noreply.github.com>
2026-09-24 23:42:53 +0530

    Time: 12 ms (35.55%) | Memory: 93.5 MB (54.47%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     int minMoves(vector<int>& nums, int limit) {
         int n=nums.size();
-        vector<int> diff(2*limit+2,0);
-       
+        vector<int> diff(2 * limit + 2,0);
+        
 
         for(int i=0;i<n/2;i++){
-            diff[2] +=2;
-            diff[2*limit+1] +=(-2);
             int a=nums[i];
             int b=nums[n-i-1];
-            int mini=min(a,b);
-            int maxi=max(a,b);
-            int l=mini+1;
-            int r=maxi+limit;
-            if(l<=2*limit) diff[l] +=(-1);
-            if(r+1<= 2*limit+1) diff[r+1] -=(-1);
-            if(a+b <=2*limit){
-                diff[a+b] +=(-1);
-                diff[a+b+1] -=(-1);
-            }
+            int mini=min(a,b)+1;
+            int maxi=max(a,b)+limit;
+            
+
+             diff[2] +=2;
+             diff[2*limit+1] -=2;
+
+            diff[mini] +=(-1);
+            diff[maxi+1] -=(-1);
+           
+            diff[a+b] +=(-1);
+            diff[a+b+1] -=(-1);
+            
         }
-        int minval=diff[2];
-        for(int i=3;i<2*limit+2-1;i++){
+        int minval=INT_MAX;
+        for(int i=2;i<=2*limit;i++){
              diff[i] +=diff[i-1];
              minval= min(minval,diff[i]);
         }
